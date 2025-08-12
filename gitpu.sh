714c1e3bf8bf3f89079bf047906545d853157db5
Zack <rubinzack40@gmail.com>
2025-08-12 19:23:46 -0400

    moved git add after message input

--- gitpu.sh
+++ gitpu.sh
@@ -1,14 +1,15 @@
 commit_and_push() {
-  git add .
   if [ -n "$1"]; then
     echo "commit message: "
     read MSG
+    git add .
     if [ -z "$MSG" ]; then
       git commit --allow-empty -m "pushing updates"
     else
       git commit -m "$MSG"
     fi
   else
+    git add .
     git commit -m "$1"
   fi
   git push
