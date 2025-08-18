aa65a272628ee2572e8d97109bc2e3c4c42a89aa
zack <rubinzack40@gmail.com>
2025-08-17 23:13:00 -0400

    migrating to new function reference

--- gitpu.sh
+++ gitpu.sh
@@ -1,3 +1,4 @@
+# until everything is migrated
 commit_and_push() {
   if [ -n "$1"]; then
     echo "commit message: "
@@ -14,3 +15,24 @@ commit_and_push() {
   fi
   git push
 }
+
+
+gitpush() {
+  if [ -n "$1"]; then
+    echo "commit message: "
+    read MSG
+    git add .
+    if [ -z "$MSG" ]; then
+      git commit --allow-empty -m "pushing updates"
+    else
+      git commit -m "$MSG"
+    fi
+  else
+    git add .
+    git commit -m "$1"
+  fi
+  git push
+}
+
+
+
